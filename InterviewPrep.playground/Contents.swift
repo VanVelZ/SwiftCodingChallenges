d688520ceb1a58bb4d0cbd1dd570f446351cc7a1
Zach <50152024+VanVelZ@users.noreply.github.com>
2022-05-16 19:23:42 -0500

    More warm up codewars

--- InterviewPrep.playground/Contents.swift
+++ InterviewPrep.playground/Contents.swift
@@ -1,13 +1,5 @@
 import Foundation
+import Darwin
 
 
-quarter(of: 1)
-quarter(of: 4)
-quarter(of: 5)
-quarter(of: 10)
 
-//1 -> 1
-//2 -> 1
-//3 -> 1
-//4 -> 1
-//5 -> 2
--- InterviewPrep.playground/Sources/CodeWars.swift
+++ InterviewPrep.playground/Sources/CodeWars.swift
@@ -40,4 +40,48 @@ public func warnTheSheep(_ queue: [String]) -> String {
     }
     return ""
 }
-
+public func rowSumOddNumbers(_ row: Int) -> Int {
+    return row * row * row
+}
+public func disemvowel(_ s: String) -> String {
+    return ["A", "E", "I", "O", "U"].reduce(s) { partial, letter in
+        partial.replacingOccurrences(of: letter, with: "", options: [.caseInsensitive])
+    }
+}
+public func sumOfPositives (_ numbers: [Int] ) -> Int {
+    numbers.reduce(0){ partial, number in
+        number > 0 ? partial + number : partial
+    }
+}
+public func bmi(_ weight: Int, _ height: Double) -> String {
+    let bmi = Double(weight) / (height * height)
+    switch(bmi) {
+      case ...18.5:
+        return "Underweight"
+      case 18.6...25.0:
+        return "Normal"
+    case 25.1...30:
+        return "Overweight"
+    case 30...:
+        return "Obese"
+    default:
+        return ""
+    }
+}
+public func firstNonConsecutive (_ arr: [Int]) -> Int? {
+    for i in 0..<(arr.count - 2) {
+        if arr[i + 1] - arr[i] != 1 {
+            return arr[i + 1]
+        }
+    }
+    return nil
+}
+public func sum_str(_ a:String, _ b:String) -> String {
+    return String(Int(a)! + Int(b)!)
+    
+}
+public func squareSum(_ vals: [Int]) -> Int {
+    vals.reduce(0) { partial, number in
+        partial + number * number
+    }
+}
